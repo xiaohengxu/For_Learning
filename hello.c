daab20602ef559eb16477f7b21fd7e20d549493d
xiaohengxu <67741709+xiaohengxu@users.noreply.github.com>
2020-07-02 21:35:10 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-  char *a;
-  a = "What's a beautiful World!\n";
-  printf("%s\n",a);
+  //char *a;
+  //a = "What's a beautiful World!\n";
+  //printf("%s\n",a);
   printf("Hello World!\n");
   return 0;
 }
